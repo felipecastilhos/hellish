e7fc495a7963501ee0e85022c343140bda711f64
Felipe Castilhos <felipecastilhos@gmail.com>
2023-08-28 20:21:15 -0300

    feat: move to running state when moving

--- Assets/Scripts/Hellish.cs
+++ Assets/Scripts/Hellish.cs
@@ -3,42 +3,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class Hellish : MonoBehaviour
-{
+using UnityEngine.Tilemaps;
+
+public class Hellish : MonoBehaviour {
     [SerializeField] float velocity = 3f;
     [SerializeField] InputActionReference movement, jump, duck;
     [SerializeField] private Animator animator;
 
-    public Rigidbody2D myRigidBody;
+    public Rigidbody2D rigidBody;
 
     private Vector2 movementDirection;
 
-    void Start()
-    {
+    void Start() {
         Debug.Log("Staring Hellish behaviour");
-        myRigidBody = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    void Update()
-    {
+    void Update() {
         Run();
     }
 
-    void Run()
-    {
+    void Run() {
         movementDirection = movement.action.ReadValue<Vector2>();
-        if (movementDirection.sqrMagnitude > 0) myRigidBody.velocity = movementDirection * velocity;
+
+        if (movementDirection.sqrMagnitude > 0) {
+            rigidBody.velocity = movementDirection * velocity;
+        }
         FlipSprite();
+        ChangeToRunningState();
     }
 
-    void FlipSprite()
-    {
-        float velocityX = myRigidBody.velocity.x;
+    private void FlipSprite() {
+        float velocityX = rigidBody.velocity.x;
         const float xScaleMultiplier = 5; //Idk why I need this to the sprite don't stretch
-        if (Mathf.Abs(velocityX) > Mathf.Epsilon)
-        {
+
+        if (Mathf.Abs(velocityX) > Mathf.Epsilon) {
             transform.localScale = new Vector3(Mathf.Sign(velocityX) * xScaleMultiplier, transform.localScale.y, transform.localScale.z);
         }
     }
 
+    private void ChangeToRunningState() { 
+        float velocityX = rigidBody.velocity.x;
+        bool isRunning = Mathf.Abs(velocityX) > Mathf.Epsilon;
+
+        animator.SetBool(HellishAnimations.Running, isRunning);
+    }
+
+
+
+}
+
+static class HellishAnimations {
+    public static readonly string Idle = "Idle";
+    public static readonly string Running = "isRunning";
+    public static readonly string Duck = "Duck";
 }
