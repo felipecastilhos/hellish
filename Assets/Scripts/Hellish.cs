db42a62987e938b95c4d002b926a0e90aa51f1bd
Felipe Castilhos <felipecastilhos@gmail.com>
2023-09-01 19:44:42 -0300

    feat: add climbing and improve hellish controls

--- Assets/Scripts/Hellish.cs
+++ Assets/Scripts/Hellish.cs
@@ -8,7 +8,7 @@ using UnityEngine.Tilemaps;
 public class Hellish : MonoBehaviour {
     [SerializeField] float velocity = 3f;
     [SerializeField] float jumpSpeed = 15F;
-    [SerializeField] InputActionReference movement, jump, duck;
+    [SerializeField] InputActionReference movement, jump, duck, climb;
     [SerializeField] private Animator animator;
 
     public Rigidbody2D rigidBody;
@@ -20,7 +20,7 @@ public class Hellish : MonoBehaviour {
     private bool isJumping;
 
     void Start() {
-        Debug.Log("Staring Hellish behaviour");
+        Debug.Log("Starting Hellish behaviour");
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         feetCollider2d = GetComponent<PolygonCollider2D>();
@@ -30,20 +30,15 @@ public class Hellish : MonoBehaviour {
     void Update() {
         Run();
         Jump();
+        Climbing();
     }
 
-void Run() {
-    movementDirection = movement.action.ReadValue<Vector2>().normalized;
-
-    if (isJumping) {
-        rigidBody.velocity = new Vector2(movementDirection.x  * velocity, rigidBody.velocity.y);
-    } else {
-        rigidBody.velocity = new Vector2(movementDirection.x * velocity, 0);
+    void Run() {
+        movementDirection = movement.action.ReadValue<Vector2>().normalized;
+        rigidBody.velocity = new Vector2(movementDirection.x * velocity, rigidBody.velocity.y);
+        FlipSprite();
+        RunningAnimationState();
     }
-
-    FlipSprite();
-    RunningAnimationState();
-}
 
     void Jump() {
         var jumpButtonPressed = jump.action.WasPressedThisFrame();
@@ -55,9 +50,26 @@ void Run() {
             Debug.Log("Hellish is jumping");
             Vector2 jumpVelocity = new(rigidBody.velocity.x, jumpSpeed);
             rigidBody.velocity = jumpVelocity;
+            isJumping = true;
+        } 
+       
+       if(canGetImpulse && rigidBody.velocity.y < Mathf.Epsilon) isJumping = false;
+
+       JumpingAnimationState(); 
+    }
+
+    void Climbing() {
+        var canClimb = capsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"));
+        var climbButtonIsPressed = climb.action.IsPressed();
+        movementDirection = movement.action.ReadValue<Vector2>();
+        var isClimbing = canClimb & climbButtonIsPressed;
+
+        if (isClimbing) {
+            Debug.Log("She is climbing");
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, movementDirection.y * velocity);
         }
 
-        JumpingAnimationState();
+        animator.SetBool(HellishAnimations.Climbing, isClimbing);
     }
 
     private void FlipSprite() {
@@ -77,9 +89,6 @@ void Run() {
     }
 
     private void JumpingAnimationState() {
-        float velocityY = rigidBody.velocity.y;
-        Debug.Log($"[JumpingAnimationState] {velocityY}");
-        isJumping = Mathf.Abs(velocityY) > Mathf.Epsilon;
         animator.SetBool(HellishAnimations.Jumping, isJumping);
     }
 }
@@ -88,4 +97,5 @@ static class HellishAnimations {
     public static readonly string Running = "isRunning";
     public static readonly string Duck = "isDucking";
     public static readonly string Jumping = "isJumping";
+    public static readonly string Climbing = "isClimbing";
 }
