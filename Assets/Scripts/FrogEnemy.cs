0a35c8f02721200d1cb4425c2cb608dc7159ab2e
Felipe Castilhos <felipecastilhos@gmail.com>
2023-09-10 09:54:52 -0300

    feat: create and move the frog enemy

--- Assets/Scripts/FrogEnemy.cs
+++ Assets/Scripts/FrogEnemy.cs
@@ -2,22 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrogEnemy : MonoBehaviour
-{
+public class FrogEnemy : MonoBehaviour {
     [SerializeField] float enemyRunSpeed = 5f;
 
-    Rigidbody2D enemyRigibody;
-    // Start is called before the first frame update
-    void Start()
-    {
-        enemyRigibody = GetComponent<Rigidbody2D>();
+    Rigidbody2D enemyRigidbody;
+    BoxCollider2D boxCollider2D;
 
+    // Start is called before the first frame update
+    void Start() {
+        enemyRigidbody = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() { }
+
+    void FixedUpdate() {
+        JumpAround();
+    }
+
+    private void JumpAround() {
         Debug.Log("Frog enemy is moving");
-        enemyRigibody.velocity = new(enemyRunSpeed, 0f);   
+        if (isFacingLeft()) {
+            enemyRigidbody.velocity = new(-enemyRunSpeed, 0f);
+        }
+        else {
+            enemyRigidbody.velocity = new(enemyRunSpeed, 0f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        Debug.Log("Frog enemy step outside the ground");
+        FlipSprite();
+        RunBack();
+    }
+
+
+    private void RunBack() {
+        enemyRigidbody.velocity = new(enemyRigidbody.velocity.x * -1, 0f);
+    }
+
+    private bool isFacingLeft() =>
+         transform.localScale.x < 0;
+
+
+    private void FlipSprite() {
+        Debug.Log($"Flip Sprite: {Mathf.Sign(enemyRigidbody.velocity.x)}");
+        float velocityX = enemyRigidbody.velocity.x;
+
+        float direction = Mathf.Sign(velocityX);
+
+        transform.localScale = new Vector3(direction * -1, 1f, 1f);
     }
 }
